fbd7ff41e0db01229e0ea35ad5cb0288639feb13
suraj <surajwakure007@gmail.com>
2024-10-20 12:52:46 +0530

    suraj

--- StudentAPI/obj/Release/net6.0/StudentAPI.AssemblyInfo.cs
+++ StudentAPI/obj/Release/net6.0/StudentAPI.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentAPI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89ccfb3c99ef8bc06bff985306ebc4e41d35da53")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentAPI")]
